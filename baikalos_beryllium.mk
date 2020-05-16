06fc61bd05e096eadad09ea3423e8da25a967f48
sdv22 <sdv22@live.ru>
2020-05-16 17:41:05 +0300

    Updarte FPs to 11.0.8

--- baikalos_beryllium.mk
+++ baikalos_beryllium.mk
@@ -19,12 +19,13 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.6.0.QEJMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.8.0.QEJMIXM:user/release-keys"
+#BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.6.0.QEJMIXM:user/release-keys"
 #BUILD_FINGERPRINT := "Xiaomi/baikalos_beryllium/beryllium:10/QQ2A.200501.001.B2/40:userdebug/test-keys"
 #BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.6.0.QEJMIXM release-keys" \
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.8.0.QEJMIXM release-keys" \
     PRODUCT_NAME="beryllium"
 
 #PRIVATE_BUILD_DESC="baikalos_beryllium-user 10 QQ2A.200405.005 6254899 release-keys"
