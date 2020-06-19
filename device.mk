03f7c606727f1f7332013a40db349e0ffbf27f83
Sergey Vasiliev <sdv22@live.ru>
2020-06-19 20:21:34 +0300

    Remove xiaomi pocket mode

--- device.mk
+++ device.mk
@@ -87,6 +87,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
-# PocketMode
-PRODUCT_PACKAGES += \
-    XiaomiPocketMode
